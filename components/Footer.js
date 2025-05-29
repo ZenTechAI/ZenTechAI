96615b46eb6a17e8945b0c91cf230d011a000232
pankaj joshi <pankaj.weby@gmail.com>
2025-05-29 04:43:19 +0000

    first Commit

--- components/Footer.js
+++ components/Footer.js
@@ -42,7 +42,7 @@ function Footer() {
               </ul>
             </div>
             <div class="copyrights mt-4">
-              © 2021 <a class='text-decoration-underline' href="https://github.com/SuccessfullSites">SuccessfullSite</a> Theme. All rights reserved
+              © 2025 <a class='text-decoration-underline' href="https://github.com/ZenTechAI">ZenTechAI</a> Theme. All rights reserved
             </div>
           </div>
           <div class="col-12 col-md-2 badges">
